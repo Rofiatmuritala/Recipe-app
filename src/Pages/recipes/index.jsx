acf7df2f8a98ca366698cf6d7026b103956dd44e
Rofiatmuritala <rofiatmuritalaa@gmail.com>
2024-02-26 12:13:36 +0000

    1. Created a recipeitem component 2. called the recipeitem in the recipes page 3. Use the apikey to get the recipes to reflect

--- src/Pages/recipes/index.jsx
+++ src/Pages/recipes/index.jsx
@@ -1,29 +1,45 @@
 import { Container, TextField, Grid, Card, CardMedia, CardContent, Typography } from "@mui/material";
+import RecipeItem from "../../components/navbar/recipe-item";
+import { useEffect, useState } from "react";
 
 
 export default function Recipes() {
+
+    const [recipes, setRecipes] = useState([]);
+
+    const searchRecipes = () => {
+        // prepare url
+        const url = new URL('https://api.spoonacular.com/recipes/complexSearch');
+        url.searchParams.append('apiKey', '2896e84b50b6471b9fb44abbc70e0a2c')
+        // fetch recipes
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            // update the recipes state
+            setRecipes(data.results);
+            // console.log(data);
+        })
+
+    .catch((error) =>{
+        console.log(error);
+    })
+        
+    }
+
+    useEffect(searchRecipes, []);
     return (
-        <Container sx={{my: '2rem'}} maxWidth="sm">
+        <Container sx={{ my: '2rem' }} >
 
             <TextField
-            fullWidth
-             id="outlined-basic" 
-             label="Enter a keyword to search recipes and hit Enter" 
-             variant="outlined" />
+                fullWidth
+                id="outlined-basic"
+                label="Enter a keyword to search recipes and hit Enter"
+                variant="outlined" />
 
-             <Grid sx={{mt: '1rem'}} container spacing={3}>
-                <Grid item xs={4}>
-                    <Card>
-                        <CardMedia
-                        sx={{height:200}}
-                        image="https://images.unsplash.com/photo-1485921325833-c519f76c4927?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-                    </Card>
-                    <CardContent>
-                        <Typography variant="h6">Recipe Name</Typography>
-                    </CardContent>
-                </Grid>
-             </Grid>
-            
+            <Grid sx={{ mt: '1rem' }} container spacing={3}>
+             {recipes.map((recipe) => <RecipeItem key={recipe.id} title={recipe.title} image={recipe.image} />)}
+            </Grid>
+
         </Container>
     );
 }
---
+++ src/components/navbar/recipe-item/index.jsx
@@ -0,0 +1,24 @@
+import { Grid, Card, CardMedia, CardContent, Typography } from "@mui/material";
+
+
+export default function RecipeItem({title,image}){
+    return (
+        <Grid item xs={4}>
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+
+                component="img"
+                // alt="green iguana"
+                height="140"
+                image={image}
+                title= "Recipe"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {title}
+                </Typography>
+            </CardContent>
+        </Card>
+    </Grid>
+    );
+}
